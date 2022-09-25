93de91d06ef9a11466f0e464e3f3857b379cc85f
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-09-25 18:21:08 +0300

    add info vc

---
+++ KhutaaScannser/ViewCodeInfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewCodeInfoVC.swift
+//  KhutaaScannser
+//
+//  Created by manar . on 25/09/2022.
+//
+
+import UIKit
+
+class ViewCodeInfoVC: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descTextField: UITextField!
+    @IBOutlet weak var codeTextField: UITextField!
+    
+    var RTitle: String = ""
+    var RDesc: String = ""
+    var RDiscount: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
